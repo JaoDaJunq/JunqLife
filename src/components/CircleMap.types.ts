775d630e1ae42653964d6a8418220d4d9b8cfefe
JaoDaJunq <jpachecohoppe@gmail.com>
2026-09-24 01:12:11 -0300

    feat: allow route overlay on circle map

--- src/components/CircleMap.types.ts
+++ src/components/CircleMap.types.ts
@@ -4,5 +4,6 @@ export type CircleMapProps = {
   members: CircleMapMember[]
   currentUserId: string
   selectedUserId?: string | null
+  routeCoordinates?: Array<[number, number]>
   onMemberPress?: (userId: string) => void
 }
